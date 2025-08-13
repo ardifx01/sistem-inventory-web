edeecf091b387d21eba35ca8312f64b503fc16fb
7Riv <riv74.sh@gmail.com>
2025-08-13 19:58:39 +0700

    delete rak salah

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,7 +21,7 @@
                         </x-nav-link>
                     </div>
                     <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                        <x-nav-link :href="route('daftar-barang')" :active="request()->routeIs('dashboard')">
+                        <x-nav-link :href="route('items.index')" :active="request()->routeIs('items.*')">
                             {{ __('Daftar Barang') }}
                         </x-nav-link>
                     </div>
