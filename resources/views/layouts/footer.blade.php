3fda757dc03a7cb9e328d2f5f88edcedbdff066e
Friand Jacnus Manuhutu <friandjacnusmanuhutu@gmail.com>
2025-08-19 10:40:07 +0700

    fixed footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,5 @@
-<footer class="bg-white dark:bg-gray-800 shadow mt-10">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 flex flex-col md:flex-row justify-between items-center text-sm text-gray-600 dark:text-gray-300">
-        <p>Sistem Gudang Branch Surabaya</p>
-        <div class="flex gap-4 mt-2 md:mt-0">
-            <a href="#" class="hover:text-indigo-600 dark:hover:text-indigo-400">About</a>
-            <a href="#" class="hover:text-indigo-600 dark:hover:text-indigo-400">Privacy</a>
-            <a href="#" class="hover:text-indigo-600 dark:hover:text-indigo-400">Contact</a>
-        </div>
-    </div>
+<footer class="fixed bottom-0 left-0 w-full bg-white dark:bg-gray-800 shadow">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex flex-col md:flex-row justify-center items-center text-sm text-gray-600 dark:text-gray-300">
+    <p>Sistem Gudang Branch Surabaya</p>
+  </div>
 </footer>
