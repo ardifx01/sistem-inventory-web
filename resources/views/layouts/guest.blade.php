a931424ce15eccf2049d44394cfdcb24646049db
Friand Jacnus Manuhutu <friandjacnusmanuhutu@gmail.com>
2025-09-01 10:53:08 +0700

    update title

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Sistem Inventory Gudang</title>
+    <link rel="icon" href="{{ asset('images/logo-bulet.png') }}" type="image/png">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
