e949f46a0321a23bc019f4bc9f2c072a148dd255
7Riv <riv74.sh@gmail.com>
2025-09-03 14:53:19 +0700

    fixed js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,24 +2,23 @@ import './bootstrap';
 import Alpine from 'alpinejs';
 import { Html5Qrcode } from 'html5-qrcode';
 import Swal from 'sweetalert2';
-
-// Import fitur-fitur
-import { initBulkActions } from './features/bulkActions';
 import { initBarcodeScanner } from './features/barcodeScanner';
+import { initBulkActions } from './features/bulkActions';
+import { initSearch } from './features/search';
 import { initCategories } from './features/categories/crud';
 import { initCategoryFilter } from './features/categories/filter';
-import { initSearch } from './features/search';
 
-// Expose libraries yang dibutuhkan ke window object
 window.Alpine = Alpine;
 window.Html5Qrcode = Html5Qrcode;
 window.Swal = Swal;
 
-// Inisialisasi fitur ketika DOM ready
 document.addEventListener("DOMContentLoaded", function () {
-    initBulkActions();
+    // Initialize all modularized features
     initBarcodeScanner();
+    initBulkActions();
+    initSearch();
     initCategories();
     initCategoryFilter();
-    initSearch();
 });
+
+Alpine.start();
